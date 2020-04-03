1e8f004b41cbb1bdb368b50c102b29068ee121a3
Steven Te <testeven152@gmail.com>
2020-04-02 17:19:03 -0700

    joinSession doesnt need videoId

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ io.on('connection', function(socket){
 
     //set sessionid to sessionid provided by user in client. 
     socket.on('joinSession', function(data, callback) {
-        if (data.sessionId in sessions && data.videoId == sessions[data.sessionId].videoId) {
+        if (data.sessionId in sessions) {
             addUserToSession(data.userId, data.sessionId);
             callback({ sessionId: users[data.userId].sessionId });
             console.log('User ' + data.userId +  ' has joined session: ' + data.sessionId + '.');
